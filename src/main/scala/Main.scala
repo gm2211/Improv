ddce347e1d2c5ff865c0421c8af977952a6ed8e4
gm2211 <gm2211@doc.ic.ac.uk>
2015-04-07 17:09:26 +0200

    Fixed dependency on trait instead of concrete interface

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,12 @@
 import instruments.OvertoneInstrumentType._
-import players.{Musician, Orchestra}
+import players.{AIOvertoneMusician, Orchestra}
 
 object Main extends App {
   val orchestra = new Orchestra()
   val instrSet = Set(PIANO, TICKER, KICK)
 
   instrSet
-    .map(Musician.createAIMusicianWithInstrType(orchestra.system, _))
+    .map(AIOvertoneMusician.createAIMusicianWithInstrType(orchestra.system, _))
     .foreach(orchestra.registerMusician)
 
   orchestra.start()
--- src/test/scala/MessagePassingTest.scala
+++ src/test/scala/MessagePassingTest.scala
@@ -6,7 +6,7 @@ import instruments.Instrument
 import messages.SyncMessage
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
-import players.AIMusician
+import players.AIOvertoneMusician
 
 import scala.concurrent.Await
 import scala.concurrent.duration.{FiniteDuration, Duration}
@@ -15,7 +15,7 @@ class MessagePassingTest extends FlatSpec with MockFactory with Matchers {
   "An actor" should "play something when a sync message is received" in {
     val instrument = mock[Instrument]
     val orchestra = ActorSystem("orchestra")
-    val musician = TestActorRef.create(orchestra, AIMusician.props(instrument), "MyMusician")
+    val musician = TestActorRef.create(orchestra, AIOvertoneMusician.props(instrument), "MyMusician")
 
     (instrument.play _).expects(*).returning().once()
     // Directly injecting message in order to avoid concurrency issues
