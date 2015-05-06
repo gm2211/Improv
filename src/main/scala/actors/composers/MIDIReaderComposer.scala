76a12985eaf76b1d81c550eab5a41e61c0779575
gm2211 <gm2211@doc.ic.ac.uk>
2015-05-06 01:07:27 +0100

    Completed builder for MIDIReaderComposer

--- src/main/scala/actors/composers/MIDIReaderComposer.scala
+++ src/main/scala/actors/composers/MIDIReaderComposer.scala
@@ -1,26 +1,33 @@
 package actors.composers
 
-import midi.MIDIParser
+import midi.{JMusicMIDIParser, MIDIParser}
 import representation.Phrase
-import utils.builders.Count
+import utils.builders.{Zero, IsOnce, Once, Count}
 
 import scala.util.Try
 
 case class MIDIReaderComposerBuilder[
-FileNameCount <: Count,
-PartNumCount <: Count](
-                        filename: String,
-                        partNum: Int,
-                        midiParser: Option[MIDIParser]) {
+  FileNameCount <: Count,
+  PartNumCount <: Count](
+    filename: Option[String] = None,
+    partNum: Option[Int] = None,
+    midiParser: Option[MIDIParser] = None){
 
+  def withFilename(filename: String) = copy[Once, PartNumCount](filename = Some(filename))
+
+  def withPartNum(partNum: Int) = copy[FileNameCount, Once](partNum = Some(partNum))
+
+  def build[A <: FileNameCount : IsOnce, B <: PartNumCount : IsOnce] = new MIDIReaderComposer(this)
 }
 
 object MIDIReaderComposer {
-
+  def builder = new MIDIReaderComposerBuilder[Zero, Zero]
 }
 
-class MIDIReaderComposer(val filename: String, val partNum: Int) extends Composer {
-  val midiReader: MIDIParser
+class MIDIReaderComposer(builder: MIDIReaderComposerBuilder[Once, Once]) extends Composer {
+  val filename: String = builder.filename.get
+  val partNum: Int = builder.partNum.get
+  val midiReader: MIDIParser = builder.midiParser.getOrElse(JMusicMIDIParser(filename))
   var phraseIterOpt: Option[Iterator[Option[Phrase]]] = Try(midiReader.getPhrases(partNum)).toOption
 
   override def compose(previousPhrase: Phrase): Phrase = {
