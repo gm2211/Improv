0b24e2c78a441cfe8ff42596495bc16ba1b1b796
gm2211 <gm2211@doc.ic.ac.uk>
2015-05-07 01:26:22 +0100

    Added more instruments to the Demo random orchestra

--- src/main/scala/demos/DemoRandomOrchestra.scala
+++ src/main/scala/demos/DemoRandomOrchestra.scala
@@ -11,7 +11,7 @@ object DemoRandomOrchestra {
   def run() = {
     val director = Option(SimpleDirector.builder.withSyncFrequencyMS(3000L))
     val orchestra = Orchestra.builder.withDirector(director).build
-    val instrSet = Set(PIANO())
+    val instrSet = Set(PIANO(), BRASS(), PERCUSSIVE(), CHROMATIC_PERCUSSION())
 
     val musicianBuilder = (instrType: InstrumentType) => {
       val instrument = new JFugueInstrument(instrumentType = instrType)
