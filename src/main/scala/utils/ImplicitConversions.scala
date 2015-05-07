e47d25f6c1a4f15ab1035ab6acc2ce46ef12ad46
gm2211 <gm2211@doc.ic.ac.uk>
2015-05-07 01:36:18 +0100

    Added implicit conversion from Java List to my enhancedtraversable

--- src/main/scala/utils/ImplicitConversions.scala
+++ src/main/scala/utils/ImplicitConversions.scala
@@ -1,13 +1,18 @@
 package utils
 
+import scala.collection.convert.{WrapAsScala, WrapAsJava}
 import scala.language.implicitConversions
 
 object ImplicitConversions {
+  import collection.JavaConversions._
   implicit def anyToRunnable[F](f: () => F): Runnable = new Runnable {
     override def run(): Unit = f()
   }
 
   implicit def wrapInOption[A <: Any](a: A): Option[A] = Option(a)
+
+  implicit def toEnhancedTraversable[A](list: java.util.List[A]): EnhancedTraversable[A] =
+    new EnhancedTraversable[A](list)
 
   implicit def toEnhancedTraversable[A](traversable: Traversable[A]): EnhancedTraversable[A] =
     new EnhancedTraversable[A](traversable)
