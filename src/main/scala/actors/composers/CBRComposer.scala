930720f9e999efb9d2506a3b44636db736c6dc96
gm2211 <gm2211@doc.ic.ac.uk>
2015-05-21 19:53:29 +0100

    Fixed method signature for cbr composer

--- src/main/scala/actors/composers/CBRComposer.scala
+++ src/main/scala/actors/composers/CBRComposer.scala
@@ -4,7 +4,7 @@ import cbr.CaseIndex
 import cbr.description.CaseDescription
 import representation.Phrase
 
-class CBRComposer(private val caseIndex: CaseIndex[CaseDescription[Phrase], Phrase]) extends Composer {
+class CBRComposer(private val caseIndex: CaseIndex[Phrase]) extends Composer {
   override def compose(phrasesByOthers: Traversable[Phrase]): Option[Phrase] = {
 //    val solutionPopulation = phrasesByOthers.flatMap(caseIndex.findKNearestNeighbours())
     None
