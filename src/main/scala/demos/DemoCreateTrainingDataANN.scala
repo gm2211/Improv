c612fb523afc85aea59ca021840ac2b71da208d5
gm2211 <gm2211@doc.ic.ac.uk>
2015-06-17 14:58:39 +0100

    Fixed db path problem when running training through gradle

--- src/main/scala/demos/DemoCreateTrainingDataANN.scala
+++ src/main/scala/demos/DemoCreateTrainingDataANN.scala
@@ -2,22 +2,40 @@ package demos
 
 import cbr.MusicalCase
 import cbr.description.PhraseDescriptionCreators
-import designPatterns.observer.{EventNotification, Observer}
+import designPatterns.observer.Observer
 import instruments.JFugueUtils
 import midi.{JMusicMIDIParser, MIDIPlayer}
 import training.DefaultMusicalCaseExtractor
 import training.ann.ANNTrainingData
 import utils.IOUtils
+import utils.collections.CollectionUtils
 
 import scala.util.Try
 
 object DemoCreateTrainingDataANN extends Observer with App {
-  val resource = scala.io.StdIn.readLine("Enter filename: ")
-  val savePath = scala.io.StdIn.readLine("Enter savePath: ")
+  val DEFAULT_DB_PATH: String = "/tmp/annDB"
+  val DEFAULT_SAVE_PATH: String = "/tmp/annData"
+
+  val resource = scala.io.StdIn.readLine("Enter music filename: ")
+
+  val savePath = {
+    val path = scala.io.StdIn.readLine(s"Enter save path for output file[default:$DEFAULT_SAVE_PATH]: ")
+    Option(path).filter(_.trim.nonEmpty).getOrElse(DEFAULT_SAVE_PATH)
+  }
+
+  val dbPath = {
+    val path = scala.io.StdIn.readLine(s"Choose dbPath [default:$DEFAULT_DB_PATH]: ")
+    Option(path).filter(_.trim.nonEmpty).getOrElse(DEFAULT_DB_PATH)
+  }
+
   val filename = IOUtils.getResourcePath(s"trainingMIDIs/$resource.mid")
 
-  run(filename, savePath)
-  def run(filename: String, savePath: String = "/tmp/dummy", fromScratch: Boolean = false) = {
+  run(filename, savePath, dbPath)
+  def run(
+      filename: String,
+      savePath: String = DEFAULT_SAVE_PATH,
+      dbLocation: String = DEFAULT_DB_PATH,
+      fromScratch: Boolean = false) = {
     val extractor = new DefaultMusicalCaseExtractor(JMusicMIDIParser)
 
     val cases = extractor.getCases(filename)
@@ -27,11 +45,17 @@ object DemoCreateTrainingDataANN extends Observer with App {
     player.addObserver(this)
 
     val descriptionCreator = PhraseDescriptionCreators.getDefault
-    val db = ANNTrainingData.loadDefaultDB
+    println(s"DB location: $dbLocation")
+    val db = ANNTrainingData.loadDB(dbLocation)
+    println("DB content: ")
+    CollectionUtils.print(db.keySet())
     val trainingData = db.get(filename).getOrElse(new ANNTrainingData(descriptionCreator.getMaxDescriptionSize, 1))
+
+    println(s"Starting from melody #${trainingData.dataPointsCount} out of ${cases.size}")
+
     if (fromScratch) trainingData.clear()
 
-    for ((_, sol) <- cases.drop(trainingData.dataPointsCount).toStream.takeWhile( _ => continue)) {
+    for ((_, sol) <- cases.toStream.drop(trainingData.dataPointsCount).takeWhile( _ => continue)) {
       val seq = JFugueUtils.toSequence(sol.phrase, sol.instrumentType)
       player.play(seq)
 
@@ -50,9 +74,10 @@ object DemoCreateTrainingDataANN extends Observer with App {
       rating.foreach(ratingVal => trainingData.addDataPoint(sig, Array(ratingVal)))
       trainingData.saveCSV(s"$savePath.csv")
 
-      db.put(filename, trainingData)
       continue = scala.io.StdIn.readLine("Continue[y/n]: ").toLowerCase != "n"
     }
+
+    db.put(filename, trainingData)
     db.commit()
   }
 
